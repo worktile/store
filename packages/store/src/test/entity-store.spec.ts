58ab2ca3468304ed9b83d8d6265fbf024f774f62
why520crazy <why520crazy@163.com>
2021-07-13 13:25:26 +0800

    test: refactore test let it be out of order

--- packages/store/src/test/entity-store.spec.ts
+++ packages/store/src/test/entity-store.spec.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@angular/core";
-import { EntityStore, EntityState, EntityStoreOptions } from "../entity-store";
+import { Injectable } from '@angular/core';
+import { EntityStore, EntityState, EntityStoreOptions } from '../entity-store';
 
-describe("Store: EntityStore", () => {
+describe('Store: EntityStore', () => {
     interface TaskInfo {
         _id: string;
         name: string;
@@ -11,17 +11,14 @@ describe("Store: EntityStore", () => {
 
     @Injectable()
     class TasksEntityStore extends EntityStore<TasksState, TaskInfo> {
-        constructor(
-            initialState?: TasksState,
-            options?: EntityStoreOptions<TaskInfo>
-        ) {
+        constructor(initialState?: TasksState, options?: EntityStoreOptions<TaskInfo>) {
             super(initialState, options);
         }
     }
 
     const initialTasks = [
-        { _id: "1", name: "task 1" },
-        { _id: "2", name: "task 2" },
+        { _id: '1', name: 'task 1' },
+        { _id: '2', name: 'task 2' }
     ];
 
     interface UserInfo {
@@ -35,47 +32,45 @@ describe("Store: EntityStore", () => {
     class UsersEntityStore extends EntityStore<UsersState, UserInfo> {
         constructor(initialState?: UsersState) {
             super(initialState, {
-                idKey: "uid",
+                idKey: 'uid'
             });
         }
     }
 
     const initialUsers = [
-        { uid: "user-1", name: "user name-1" },
-        { uid: "user-2", name: "user name-2" },
+        { uid: 'user-1', name: 'user name-1' },
+        { uid: 'user-2', name: 'user name-2' }
     ];
 
-    it("should get store default value", () => {
+    it('should get store default value', () => {
         const taskEntityStore = new TasksEntityStore();
         const state = taskEntityStore.snapshot;
         expect(state.pagination).toEqual(undefined);
         expect(state.entities).toEqual([]);
 
-        expect(
-            taskEntityStore.trackBy(0, { _id: "111", name: "name1" })
-        ).toEqual("111");
+        expect(taskEntityStore.trackBy(0, { _id: '111', name: 'name1' })).toEqual('111');
     });
 
-    it("should get initialize data when call store initialize", () => {
+    it('should get initialize data when call store initialize', () => {
         const taskEntityStore = new TasksEntityStore();
         taskEntityStore.initialize([...initialTasks], {
             pageIndex: 2,
             pageSize: 20,
-            pageCount: 20,
+            pageCount: 20
         });
         const state = taskEntityStore.snapshot;
         expect(state.entities).toEqual([
-            { _id: "1", name: "task 1" },
-            { _id: "2", name: "task 2" },
+            { _id: '1', name: 'task 1' },
+            { _id: '2', name: 'task 2' }
         ]);
         expect(state.pagination).toEqual({
             pageIndex: 2,
             pageSize: 20,
-            pageCount: 20,
+            pageCount: 20
         });
     });
 
-    describe("add", () => {
+    describe('add', () => {
         let tasksEntityStore: TasksEntityStore;
 
         beforeEach(() => {
@@ -84,54 +79,54 @@ describe("Store: EntityStore", () => {
                 pagination: {
                     pageIndex: 1,
                     pageSize: 10,
-                    count: initialTasks.length,
-                },
+                    count: initialTasks.length
+                }
             });
         });
 
-        it("should task success for append", () => {
+        it('should task success for append', () => {
             const addEntity = {
-                _id: "3",
-                name: "task 3",
+                _id: '3',
+                name: 'task 3'
             };
             tasksEntityStore.add(addEntity);
             const state = tasksEntityStore.snapshot;
             expect(state.entities).toEqual([...initialTasks, addEntity]);
         });
 
-        it("should add task success for prepend=true", () => {
+        it('should add task success for prepend=true', () => {
             const addEntity = {
-                _id: "3",
-                name: "task 3",
+                _id: '3',
+                name: 'task 3'
             };
             tasksEntityStore.add(addEntity, {
-                prepend: true,
+                prepend: true
             });
             const state = tasksEntityStore.snapshot;
             expect(state.entities).toEqual([addEntity, ...initialTasks]);
         });
 
-        it("should add task 3 success to afterId", () => {
+        it('should add task 3 success to afterId', () => {
             const addEntity = {
-                _id: "3",
-                name: "task 3",
+                _id: '3',
+                name: 'task 3'
             };
             tasksEntityStore.add(addEntity, {
-                afterId: "1",
+                afterId: '1'
             });
             const state = tasksEntityStore.snapshot;
             expect(state.entities).toEqual([
-                { _id: "1", name: "task 1" },
+                { _id: '1', name: 'task 1' },
                 {
-                    _id: "3",
-                    name: "task 3",
+                    _id: '3',
+                    name: 'task 3'
                 },
-                { _id: "2", name: "task 2" },
+                { _id: '2', name: 'task 2' }
             ]);
         });
     });
 
-    describe("remove", () => {
+    describe('remove', () => {
         let tasksEntityStore: TasksEntityStore;
 
         beforeEach(() => {
@@ -141,8 +136,8 @@ describe("Store: EntityStore", () => {
                     pageCount: 1,
                     pageIndex: 1,
                     pageSize: 10,
-                    count: 2,
-                },
+                    count: 2
+                }
             });
         });
 
@@ -153,42 +148,39 @@ describe("Store: EntityStore", () => {
                 pageCount: 1,
                 pageIndex: 1,
                 pageSize: 10,
-                count: 2,
+                count: 2
             });
-            tasksEntityStore.remove("1");
+            tasksEntityStore.remove('1');
             expect(state.pagination).toEqual({
                 pageCount: 1,
                 pageIndex: 1,
                 pageSize: 10,
-                count: 1,
+                count: 1
             });
             expect(state.entities).toEqual(
                 initialTasks.filter((item) => {
-                    return item._id !== "1";
+                    return item._id !== '1';
                 })
             );
-            expect(originalEntities === state.entities).toEqual(
-                false,
-                "new state is immutable"
-            );
+            expect(originalEntities === state.entities).toEqual(false, 'new state is immutable');
         });
 
         it(`remove by ids`, () => {
-            tasksEntityStore.remove(["1", "2"]);
+            tasksEntityStore.remove(['1', '2']);
             const state = tasksEntityStore.snapshot;
             expect(state.entities).toEqual([]);
         });
 
         it(`remove by predicate`, () => {
             tasksEntityStore.remove((entity) => {
-                return entity._id === "1";
+                return entity._id === '1';
             });
             const state = tasksEntityStore.snapshot;
             expect(state.entities).toEqual([
                 {
-                    _id: "2",
-                    name: "task 2",
-                },
+                    _id: '2',
+                    name: 'task 2'
+                }
             ]);
         });
 
@@ -197,7 +189,7 @@ describe("Store: EntityStore", () => {
             for (let index = 0; index < 11; index++) {
                 userEntities.push({
                     _id: `${index + 1}`,
-                    name: `${index + 1} name`,
+                    name: `${index + 1} name`
                 });
             }
             tasksEntityStore = new TasksEntityStore({
@@ -206,8 +198,8 @@ describe("Store: EntityStore", () => {
                     pageCount: 2,
                     pageIndex: 1,
                     pageSize: 10,
-                    count: 11,
-                },
+                    count: 11
+                }
             });
             const state = tasksEntityStore.snapshot;
             const originalPagination = state.pagination;
@@ -215,141 +207,133 @@ describe("Store: EntityStore", () => {
                 pageCount: 2,
                 pageIndex: 1,
                 pageSize: 10,
-                count: 11,
+                count: 11
             });
-            tasksEntityStore.remove("1");
+            tasksEntityStore.remove('1');
             expect(state.pagination).toEqual({
                 pageCount: 1,
                 pageIndex: 1,
                 pageSize: 10,
-                count: 10,
+                count: 10
             });
-            expect(originalPagination === state.pagination).toEqual(
-                false,
-                "new pagination is immutable"
-            );
+            expect(originalPagination === state.pagination).toEqual(false, 'new pagination is immutable');
         });
 
         it(`remove user by custom idKey uid`, () => {
             const userStore = new UsersEntityStore({
-                entities: initialUsers,
+                entities: initialUsers
             });
             const state = userStore.snapshot;
             expect(state.entities).toEqual([...initialUsers]);
-            userStore.remove("user-1");
-            expect(state.entities).toEqual([
-                { uid: "user-2", name: "user name-2" },
-            ]);
+            userStore.remove('user-1');
+            expect(state.entities).toEqual([{ uid: 'user-2', name: 'user name-2' }]);
         });
     });
 
-    describe("update", () => {
+    describe('update', () => {
         let tasksEntityStore: TasksEntityStore;
 
         beforeEach(() => {
             tasksEntityStore = new TasksEntityStore({
                 entities: [...initialTasks],
-                pagination: null,
+                pagination: null
             });
         });
 
         it(`update by id`, () => {
             const originalEntities = tasksEntityStore.snapshot.entities;
-            tasksEntityStore.update("1", {
-                name: "new 1 user",
+            tasksEntityStore.update('1', {
+                name: 'new 1 user'
             });
             const state = tasksEntityStore.snapshot;
             const entity = state.entities.find((item) => {
-                return item._id === "1";
+                return item._id === '1';
             });
             expect(entity).toEqual({
-                _id: "1",
-                name: "new 1 user",
+                _id: '1',
+                name: 'new 1 user'
             });
-            expect(originalEntities === state.entities).toEqual(
-                false,
-                "new state is immutable"
-            );
+            expect(originalEntities === state.entities).toEqual(false, 'new state is immutable');
         });
 
         it(`update by ids`, () => {
-            tasksEntityStore.update(["1", "2"], {
-                name: "new 1 user",
+            tasksEntityStore.update(['1', '2'], {
+                name: 'new 1 user'
             });
             const state = tasksEntityStore.snapshot;
 
             expect(state.entities).toEqual([
                 {
-                    _id: "1",
-                    name: "new 1 user",
+                    _id: '1',
+                    name: 'new 1 user'
                 },
                 {
-                    _id: "2",
-                    name: "new 1 user",
-                },
+                    _id: '2',
+                    name: 'new 1 user'
+                }
             ]);
         });
 
         it(`update by id and newStateFn`, () => {
-            tasksEntityStore.update("1", (user) => {
+            tasksEntityStore.update('1', (user) => {
                 return {
                     ...user,
-                    name: "new 1 user",
+                    name: 'new 1 user'
                 };
             });
             const state = tasksEntityStore.snapshot;
             const entity = state.entities.find((item) => {
-                return item._id === "1";
+                return item._id === '1';
             });
             expect(entity).toEqual({
-                _id: "1",
-                name: "new 1 user",
+                _id: '1',
+                name: 'new 1 user'
             });
         });
 
         it(`update user by custom idKey uid`, () => {
             const userStore = new UsersEntityStore({
-                entities: initialUsers,
+                entities: initialUsers
             });
             const state = userStore.snapshot;
             expect(state.entities).toEqual([...initialUsers]);
-            userStore.update("user-1", {
-                name: "new user1 name",
+            userStore.update('user-1', {
+                name: 'new user1 name'
             });
             expect(state.entities).toEqual([
-                { uid: "user-1", name: "new user1 name" },
-                { uid: "user-2", name: "user name-2" },
+                { uid: 'user-1', name: 'new user1 name' },
+                { uid: 'user-2', name: 'user name-2' }
             ]);
         });
     });
 
-    describe("clear", () => {
-        it("should clear entities and pagination", () => {
+    describe('clear', () => {
+        it('should clear entities and pagination', () => {
             const tasksEntityStore = new TasksEntityStore({
                 entities: [...initialTasks],
-                pagination: { pageIndex: 1, count: 100, pageSize: 20 },
+                pagination: { pageIndex: 1, count: 100, pageSize: 20 }
             });
             expect(tasksEntityStore.snapshot.entities).toEqual(initialTasks);
             expect(tasksEntityStore.snapshot.pagination).toEqual({
                 pageIndex: 1,
                 count: 100,
-                pageSize: 20,
+                pageSize: 20
             });
             tasksEntityStore.clear();
             expect(tasksEntityStore.snapshot.entities).toEqual([]);
             expect(tasksEntityStore.snapshot.pagination).toEqual(null);
         });
 
-        it("should clear pagination", () => {
+        it('should clear pagination', () => {
             const tasksEntityStore = new TasksEntityStore({
                 entities: [...initialTasks],
-                pagination: { pageIndex: 1, count: 100, pageSize: 20 },
+                pagination: { pageIndex: 1, count: 100, pageSize: 20 }
             });
             expect(tasksEntityStore.snapshot.entities).toEqual(initialTasks);
             expect(tasksEntityStore.snapshot.pagination).toEqual({
                 pageIndex: 1,
                 count: 100,
-                pageSize: 20,
+                pageSize: 20
             });
             tasksEntityStore.clearPagination();
             expect(tasksEntityStore.snapshot.entities).toEqual(initialTasks);
@@ -357,35 +341,35 @@ describe("Store: EntityStore", () => {
         });
     });
 
-    describe("constructor", () => {
-        it("should merge default options", () => {
+    describe('constructor', () => {
+        it('should merge default options', () => {
             const store = new TasksEntityStore(
                 {
-                    entities: [...initialTasks],
+                    entities: [...initialTasks]
                 },
                 {}
             );
-            expect(store["options"]).toEqual({
-                idKey: "_id",
+            expect(store['options']).toEqual({
+                idKey: '_id'
             });
         });
 
-        it("should throw error when idKey is empty", () => {
+        it('should throw error when idKey is empty', () => {
             const assertIdKeyError = function (idKeyValue: string) {
                 expect(() => {
                     return new TasksEntityStore(
                         {
-                            entities: [...initialTasks],
+                            entities: [...initialTasks]
                         },
                         {
-                            idKey: idKeyValue as any,
+                            idKey: idKeyValue as any
                         }
                     );
-                }).toThrowError("idKey is required in EntityStore");
+                }).toThrowError('idKey is required in EntityStore');
             };
             assertIdKeyError(undefined);
             assertIdKeyError(null);
-            assertIdKeyError("");
+            assertIdKeyError('');
         });
     });
 });
