8582975b0f76981ae714206a0028009838d9ec0d
why520crazy <why520crazy@users.noreply.github.com>
2022-05-17 16:43:32 +0800

    build: bump @worktile/pkg-manager to 0.0.9 and update yarn registry to npm (#34)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    extends: ['@commitlint/config-angular']
+    extends: ['@commitlint/config-angular'],
+    rules: {
+        'header-max-length': [0, 'always', 120],
+        'scope-enum': [2, 'always', ['store', 'action', 'deps']]
+    }
 };
