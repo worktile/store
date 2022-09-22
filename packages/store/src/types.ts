c7f513ddb1ff02dee29e4a10ddb287b28bcf3643
why520crazy <why520crazy@163.com>
2022-09-22 12:31:15 +0800

    refactor: update comment

--- packages/store/src/types.ts
+++ packages/store/src/types.ts
@@ -17,14 +17,17 @@ export interface PaginationInfo {
 
 // export type Newable<T> = { new (...args: any[]): T };
 
+/**
+ * Store options
+ */
 export interface StoreOptions {
     /**
      * Define name of store, default name is generated according to the class name, e.g. class ZoomStore name is `ZoomStore`
      */
     name?: string;
     /**
-     * The max number of instances of the current store, set to 0 means unlimited
-     * Note: only throw error devMode, unlimited in production environment
+     * The max number of instances for the current store, set to 0 means unlimited
+     * Note: only throw error in devMode, unlimited in production environment
      * @default 20
      */
     instanceMaxCount?: number;
