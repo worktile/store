acb4b9dd7d668047768c1807dace19e07322b21f
why520crazy <why520crazy@163.com>
2023-01-31 16:28:34 +0800

    test: use Angular DI inject stores for all test #72

--- packages/store/test/store-with-options.spec.ts
+++ packages/store/test/store-with-options.spec.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken, StaticProvider } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { EntityState, EntityStore } from '../entity-store';
 import { Store } from '../store';
@@ -58,7 +58,7 @@ describe('store-with-options', () => {
     it('should throw errors when store instance count > 20', () => {
         const destroy = createStores(20, GardenStore);
         expect(() => {
-            new GardenStore();
+            TestBed.inject(GardenStore);
         }).toThrowError(`store 'garden' created more than 20, please check it.`);
         destroy();
     });
@@ -66,7 +66,7 @@ describe('store-with-options', () => {
     it('should create store instance by custom limited count, instanceMaxCount = 50', () => {
         const destroy = createStores(50, GardenStoreWithCustomLimitedCount);
         expect(() => {
-            new GardenStoreWithCustomLimitedCount();
+            TestBed.inject(GardenStoreWithCustomLimitedCount);
         }).toThrowError(`store 'GardenStoreWithCustomLimitedCount' created more than 50, please check it.`);
         destroy();
     });
@@ -74,15 +74,32 @@ describe('store-with-options', () => {
     it('should create store instance without limited count, instanceMaxCount = 0', () => {
         const destroy = createStores(500, GardenStoreWithUnlimitedCount);
         destroy();
-        expect(true);
+        expect(true).toBe(true);
     });
 
     function createStores(count: number, store: { new (...args: any[]): Store }) {
         const stores: Store[] = [];
+        TestBed.resetTestingModule();
+        const TOKEN = new InjectionToken('token');
+        TestBed.configureTestingModule({
+            providers: [
+                GardenStore,
+                GardensStore,
+                GardenStoreWithCustomLimitedCount,
+                {
+                    provide: TOKEN,
+                    useFactory: () => {
+                        for (let i = 0; i < count; i++) {
+                            stores.push(new store());
+                        }
+                        return stores;
+                    }
+                }
+            ]
+        });
 
-        for (let i = 0; i < count; i++) {
-            stores.push(new store());
-        }
+        TestBed.inject(TOKEN);
+
         return () => {
             stores.forEach((store) => {
                 store.ngOnDestroy();
