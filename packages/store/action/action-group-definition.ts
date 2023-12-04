8580ef36df2a42805a379a60ea570d45a9f810d2
MissLixf <1090474924@qq.com>
2023-12-04 18:44:37 +0800

    refactor: code review

--- packages/store/action/action-group-definition.ts
+++ packages/store/action/action-group-definition.ts
@@ -6,15 +6,25 @@ type PayloadRef = { [payloadSymbol]: true };
 
 type ExtractActionCreators<T> = { [key in keyof T]?: ActionCreator<T[key] extends { payload: infer P } ? P : T[key]> };
 
+function isPayloadRef(variable: unknown): variable is PayloadRef {
+    return typeof variable === 'object' && payloadSymbol in variable && variable[payloadSymbol] === true;
+}
+
 export function payload<T1 = never, T2 = never, T3 = never, T4 = never>() {
-    return null as unknown as PayloadRef & { payload: ExtractTypeToPayload<[T1, T2, T3, T4]> };
+    return {
+        [payloadSymbol]: true
+    } as PayloadRef & { payload: ExtractTypeToPayload<[T1, T2, T3, T4]> };
 }
 
 export function defineActions<T extends Record<string, PayloadRef>>(groupName: string, actions: T) {
     let result: ExtractActionCreators<T> = {};
     for (const key in actions) {
-        const type = `${groupName}_${key}`;
-        result[key] = defineAction(type) as ActionCreator;
+        if (isPayloadRef(actions[key])) {
+            const type = `${groupName}_${key}`;
+            result[key] = defineAction(type) as ActionCreator;
+        } else {
+            throw new Error(`${key} is not a PayloadRef, please use payload function define it.`);
+        }
     }
     return result;
 }
